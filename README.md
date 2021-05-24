36aea8d434190c17ae37f5630270705c5bf2e8bc
kodiakshuksan <74173891+kodiakshuksan@users.noreply.github.com>
2021-05-24 14:26:44 -0700

    made included array the length of the password

--- README.md
+++ README.md
@@ -2,6 +2,19 @@
 
 by Jennifer Detmering
 
+#### [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)]
+
+## Table of Contents
+
+1. [Installation](#installation)
+2. [Usage](#usage)
+3. [Technologies Used](#technologies_used)
+4. [Credits](#credits)
+5. [License](#license)
+6. [Features](#features)
+7. [Questions and Contributions](#questions_and_contributions)
+8. [Tests](#tests)
+
 ## Description
 
 This is basic Javascript password generator program. It generates a new password once the user chooses the length and whether or not to include Uppercase letters, lowercase letters, numbers, and/or special characters.
@@ -10,10 +23,11 @@ This is basic Javascript password generator program. It generates a new password
 
 ## https://kodiakshuksan.github.io/PassWordTryAgain/
 
-## Project Image
+## Installation
 
-![transactions](code.png)
-![transactions](pass.png)
+![code](code.png)
+
+![password](password.png)
 
 ## Technologies
 
@@ -29,7 +43,28 @@ For this project I used Visual Studio Code, HTML, CSS, and Javascript.
 getting ready to apply alerts, prompts, confirms
 1/29/2021-was able to get working though the length and prompts matching up with values. Was able to push this info into a
 password array for temp.
+5/24/2021-Finally got the included array to be the length of designated password
 
-## Contributors
+## Credits-
 
-https://stackoverflow.com/questions/10952615/how-can-i-find-the-length-of-a-number
+[w3 schools](https://w3schools.com)
+
+## License-
+
+### [License: MIT](https://opensource.org/licenses/MIT)
+
+## Features-
+
+Uses Google API Books to search for books and save them to a reading wish list.
+
+## Questions and Contributions-
+
+[![Contributor Covenant](https://img.shields.io/badge/Contributor%20Covenant-2.0-4baaaa.svg)](code_of_conduct.md)
+
+### GitHub Profile: https://github.com/kodiakshuksan
+
+### Email: kodiakshuksan@yahoo.com
+
+## Tests-
+
+none
--- script2.js
+++ script2.js
@@ -1,45 +1,5 @@
-// //New JavaScript
-
-// //showing Javascript is Connected
-// console.log("hello")
-
-
-
-// //NUMBER
-// var number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-// var randomNumber4 = Math.floor(Math.random() * 10)
-// console.log(number[randomNumber4])
-// var choiceNumber = number[randomNumber4];
-// console.log(choiceNumber)
-
-
-// //LOWERCASE
-// var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-// var randomNumber2 = Math.floor(Math.random() * 26)
-// console.log(lowercase[randomNumber2])
-// var choiceLowercase = lowercase[randomNumber2];
-// console.log(choiceLowercase)
-
-// //UPPERCASE
-// var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-// var randomNumber3 = Math.floor(Math.random() * 26)
-// console.log(uppercase[randomNumber3])
-// var choiceUppercase = uppercase[randomNumber3];
-// //included.push(choiceUppercase)
-// console.log(choiceUppercase)
-
-// //SYMBOLS
-// var symbols = ["!", "@", "#", "$", "%", "^", "&", "*"];
-// console.log(symbols[0]);
-
-// // //generate a random number between 0 and 7
-// var randomNumber1 = Math.floor(Math.random() * 8);
-
-// //generates random symbol
-// console.log(symbols[randomNumber1])
-// console.log(randomNumber1)
-// var choiceSymbols = (symbols[randomNumber1]);
-// console.log(choiceSymbols)
+function generatePassword() {
+    // //New JavaScript
 
 
 
@@ -99,7 +59,17 @@ var usersChoices = [number + lowercase + uppercase + symbols];
 console.log(usersChoices)
 console.log(questionBox2)
 
-//how the password is created and collected in the console after the prompts
+
+
+
+
+console.log(included)
+console.log(lengthOfPassword)
+
+var text = "";
+var i;
+for (i=0; i < lengthOfPassword; i++) {
+    //how the password is created and collected in the console after the prompts
 if (questionBox2 === true) {
     var number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
@@ -167,56 +137,23 @@ if (questionBox2 === true) {
 
 
 };
-console.log(included)
-console.log(lengthOfPassword)
-const goal = lengthOfPassword;
-console.log(goal)
-var text = "";
-var i;
-for (i = 0; i < included.length; i++) {
+}
+for (i = 0; i < lengthOfPassword; i++) {
   text += included[i];
   console.log(i)
 }
 
-// for (let i = 0; i < included.length; i++) {
-//     text += included[i]}
-//text = [randoms];
-//var randoms = Array(goal).fill(0).map(included);
+
   
 //console.log(randoms)
-console.log(goal)
 console.log(text)
 document.getElementById("password").innerHTML = text;
 
-// work on length of password 
-// make password
-// displaying password
 
 
 
+}
+//generatePassword();
 
-//document.getElementById("generate").addEventListener("click", included//);
-
-
-
-
-
-
-
-
-    // lengthOfPassword.toString().length;
-
-    // console.log(lengthOfPassword)
-
-
-
-    // //var practice = usersChoices + password part 
-    // console.log(practice)
-
-
-
-
-
-
-
-
+const generateButton = document.getElementById("generate");
+generateButton.addEventListener("click", generatePassword);
