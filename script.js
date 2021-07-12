8881a6134d5f86da984b670691f6b1b83ec7e3b1
kodiakshuksan <74173891+kodiakshuksan@users.noreply.github.com>
2021-07-12 12:28:39 -0700

    working on including a number and alerts/fixing crashing issue

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ console.log(lengthOfPassword)
 
 //prompt for Must Include a Number
 var questionBox2 = window.confirm('Must include a number!');
-console.log('Do you want to include a number?')
+console.log('Must include a number?')
 console.log(questionBox2);
 
 //prompt for choosing to include LOWERCASE LETTER
@@ -67,40 +67,8 @@ var i;
 for (i=0; i < lengthOfPassword; i++) {
 
     //how the password is created and collected in the console after the prompts
-if (questionBox2 === true) {
-    var number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-    included.push(number[Math.floor(Math.random() * 10)])
-
-    if (questionBox3 === true) {
-
-        var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-
-        included.push(lowercase[Math.floor(Math.random() * 26)])
-        console.log(included)
-    }
-
-    if (questionBox4 === true) {
-
-        var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-
-        included.push(uppercase[Math.floor(Math.random() * 26)])
-        console.log(included)
-    }
-
-
-    if (questionBox5 === true) {
-        var symbols = ["!", "@", "#", "$", "%", "^", "&", "*"];
-
-        included.push(symbols[Math.floor(Math.random() * 8)])
-        console.log(included)
-    }
-} else {
-    alert('Must include Number!')
-
 
     var number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-
-
     included.push(number[Math.floor(Math.random() * 10)])
 
 
@@ -127,20 +95,26 @@ if (questionBox2 === true) {
         included.push(symbols[Math.floor(Math.random() * 8)])
         console.log(included)
     }
+
 };
-}
+
+
 for (i = 0; i < lengthOfPassword; i++) {
   text += included[i];
   console.log(i)
 }
 
-
+//Checking the value of text
 console.log(text)
+
+//Inserts text in the Element with the ID "password"
 document.getElementById("password").innerHTML = text;
 }
 
 //generatePassword();
 
-//Button Generates Password
+//Button Generates Password- Button Identified by using ID
 const generateButton = document.getElementById("generate");
+
+//Listens for Button to Click, Runs code when Clicked
 generateButton.addEventListener("click", generatePassword);
